57ededf64b29321e48e7fe83b4151ae04c89b172
angeldvilla <escarlata.2003@gmail.com>
2023-10-15 23:52:54 -0500

    Add changes

--- src/views/Home/home.jsx
+++ src/views/Home/home.jsx
@@ -1,9 +1,11 @@
 import Atropos from "atropos/react";
+import NavBar from "../../components/NavBar/navBar";
 
 const home = () => {
   return (
     <div id="app">
-      <Atropos className="my-atropos">
+        <NavBar/>
+      <Atropos className="my-atropos" style={{marginTop: "2em"}}>
         <h1>BIENVENIDOS!! SOY ANGEL VILLA</h1>
         <h3>DESARROLLADOR FRONT-END & FULL STACK</h3>
       </Atropos>
